e93e5141dc55cff08e8550f1051e77454a0dada5
Ushakov Ilya <Riz4rd@gmail.com>
2022-11-15 18:42:08 +0500

    add Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -7,8 +7,19 @@
 Console.WriteLine("Введите число ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = -number;
+if (number >= 0)
+{
 while(count <= number)
 {
     Console.WriteLine(count);
     count++;
+}
+}
+else
+{
+    while (count >= number)
+    {
+        Console.WriteLine(count);
+        count--;
+    }
 }
